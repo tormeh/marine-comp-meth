13bb2cfd833c992cde8855e3176fa06a32eee4d1
Tormod Hellen <tormod.hellen@gmail.com>
2015-03-15 20:35:11 +0100

    almost compiling

--- poisson-solver/solver.f95
+++ poisson-solver/solver.f95
@@ -1,11 +1,47 @@
+      
+      
+      
+      
+      FUNCTION BOUNDARY(X,Y,H)
+        INTEGER :: X
+        INTEGER :: Y
+        REAL :: H
+        BOUNDARY = (1/4.0)*(X*X*H*H+Y*Y*H*H)
+        RETURN
+      END FUNCTION
+      
+      
+      
+      FUNCTION F(X,Y)
+        INTEGER X,Y
+        F=1.0
+        RETURN
+      END FUNCTION
+      
+      
+      FUNCTION NEWESTIMATE(X,Y,H)
+        INTEGER X,Y
+        REAL H
+        NEWESTIMATE = (1/4.0)*(PHI(X+1,Y)+PHI(X,Y+H)+PHI(X-H,Y)+PHI(X,Y-H)-H*H*F(X,Y))
+        RETURN
+      END FUNCTION
+      
+      FUNCTION SOLVE(LENGTH)
+        INTEGER :: LENGTH
+      END FUNCTION
+      
       PROGRAM SOLVER
-      
-      REAL FUNCTION F(X,Y)
-      F=1.0
-      RETURN
-      
-      REAL FUNCTION PHI(X,Y,H)
-      REAL X,Y,H
-      PHIESTIMATE = (1/4.0)*(PHI(X+H,Y)+PHI(X,Y+H)+PHI(X-H,Y)+PHI(X,Y-H)-H*H*F(X,Y))
-      RETURN
+        PARAMETER (H = 0.1)
+        PARAMETER (LENGTH = (1.0/H)+1)
+        REAL :: PHIS(LENGTH, LENGTH)
+        
+        DO I=1,LENGTH
+          DO J=1,LENGTH
+              IF (I==1 .OR. I==LENGTH .OR. J==1 .OR. j==LENGTH) THEN
+                PHIS(I,J) = BOUNDARY(I,J,H)
+              ELSE
+                PHIS(I,J) = NEWESTIMATE(I,J,H)
+              END IF
+          END DO
+        END DO
       END
