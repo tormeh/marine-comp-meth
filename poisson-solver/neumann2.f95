dab253fa392f444eb40d03496c1ee452a995ffa3
Tormod Hellen <tormod.hellen@gmail.com>
2015-03-31 17:40:50 +0200

    better measurement of convergence

--- poisson-solver/neumann2.f95
+++ poisson-solver/neumann2.f95
@@ -13,32 +13,50 @@
         INTEGER  Y
         REAL H
         INTEGER LENGTH
+        REAL NEWVALUE
         REAL :: PHIS(LENGTH,LENGTH)
         REAL BOUNDARY
         REAL NEWESTIMATE
+        REAL F2
         IF (X==1 .AND. Y==1) THEN
+          PHIS(1,2)=0.0
+          PHIS(2,1)=0.0
+          PHIS(2,2)=0.0
           BOUNDARY=0.0
         ELSE IF (X==1 .AND. Y==LENGTH) THEN
-          BOUNDARY = NEWESTIMATE(X,Y,H,PHIS,LENGTH)
+          PHIS(2,LENGTH) = PHIS(2,LENGTH-1)
+          PHIS(1,LENGTH-1) = PHIS(2,LENGTH-1)
+          BOUNDARY = PHIS(2,LENGTH-1)
         ELSE IF (X==LENGTH .AND. Y==1) THEN
-          BOUNDARY = NEWESTIMATE(X,Y,H,PHIS,LENGTH)
+          PHIS(LENGTH-1,1) = PHIS(LENGTH-1,2)
+          PHIS(LENGTH,2) = PHIS(LENGTH-1,2)
+          BOUNDARY = PHIS(LENGTH-1,2)
         ELSE IF (X==LENGTH .AND. Y==LENGTH) THEN
-          BOUNDARY = NEWESTIMATE(X,Y,H,PHIS,LENGTH)
+          PHIS(LENGTH-1,LENGTH) = PHIS(LENGTH-1,LENGTH-1)
+          PHIS(LENGTH,LENGTH-1) = PHIS(LENGTH-1,LENGTH-1)
+          BOUNDARY = PHIS(LENGTH-1,LENGTH-1)
         ELSE IF (X==1) THEN
-          BOUNDARY = PHIS(2,Y)
+          NEWVALUE = (1.0/2.0)*(PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F2(X,Y,H))
+          PHIS(X+1,Y) = NEWVALUE
+          BOUNDARY = NEWVALUE
         ELSE IF (X==LENGTH) THEN
-          BOUNDARY = PHIS(LENGTH-1,Y)
+          NEWVALUE = (1.0/2.0)*(PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F2(X,Y,H))
+          PHIS(X-1,Y) = NEWVALUE
+          BOUNDARY = NEWVALUE
         ELSE IF (Y==1) THEN
-          BOUNDARY = PHIS(X,2)
+          NEWVALUE = (1.0/2.0)*(PHIS(X+1,Y)+PHIS(X-1,Y)-H*H*F2(X,Y,H))
+          PHIS(X,Y+1) = NEWVALUE
+          BOUNDARY = NEWVALUE
         ELSE IF (Y==LENGTH) THEN
-          BOUNDARY = PHIS(X,LENGTH-1)
+          NEWVALUE = (1.0/2.0)*(PHIS(X+1,Y)+PHIS(X-1,Y)-H*H*F2(X,Y,H))
+          PHIS(X,Y-1) = NEWVALUE
+          BOUNDARY = NEWVALUE
         ELSE
           BOUNDARY = NEWESTIMATE(X,Y,H,PHIS,LENGTH)
         END IF
         RETURN
       END FUNCTION
            
-      
       
       FUNCTION NEWESTIMATE(X,Y,H,PHIS,LENGTH)
         INTEGER X,Y
@@ -68,9 +86,21 @@
         RETURN
       END FUNCTION
       
+      FUNCTION LOWCHANGE(OLDLOWEST, LASTLOW)
+        REAL LOWCHANGE
+        REAL OLDLOWEST
+        REAL LASTLOW
+        IF (OLDLOWEST .LT. LASTLOW) THEN
+          LOWCHANGE = OLDLOWEST
+        ELSE
+          LOWCHANGE = LASTLOW
+        END IF
+      END FUNCTION
+      
       PROGRAM SOLVER
         REAL, PARAMETER :: H = 0.1
         INTEGER, PARAMETER :: LENGTH = (1.0/H)+1
+        INTEGER, PARAMETER :: SIZE = LENGTH*LENGTH
         REAL :: PHIS(LENGTH, LENGTH)
         INTEGER, PARAMETER :: out_unit=20
         REAL :: HIGHESTCHANGE
@@ -79,16 +109,26 @@
         REAL BOUNDARY
         INTEGER I,J
         REAL HIGHESTCHANGEFUN
-        HIGHESTCHANGE = 0.5
+        REAL LOWHIGHESTCHANGE
+        REAL AVGCHANGE
+        REAL LOWAVGCHANGE
+        REAL LOWCHANGE
+        LOWAVGCHANGE = 20.0
+        AVGCHANGE = 1.0
+        LOWHIGHESTCHANGE = 20.0
+        HIGHESTCHANGE = 10.0
         
         DO I=1,LENGTH
           DO J=1,LENGTH
-            PHIS(I,J) = 0.0
+            PHIS(I,J) = RAND(0)*10
           END DO
         END DO
         
-        DO WHILE (HIGHESTCHANGE > 0.015)
+        DO WHILE ((LOWHIGHESTCHANGE/HIGHESTCHANGE) > 1.0 .OR. (LOWAVGCHANGE/AVGCHANGE) > 1.0)
+          LOWAVGCHANGE = LOWCHANGE(LOWAVGCHANGE, AVGCHANGE)
+          LOWHIGHESTCHANGE = LOWCHANGE(LOWHIGHESTCHANGE, HIGHESTCHANGE)
           HIGHESTCHANGE = 0.0
+          AVGCHANGE = 0.0
           DO I=1,LENGTH
             DO J=1,LENGTH
                 IF (I==1 .OR. I==LENGTH .OR. J==1 .OR. J==LENGTH) THEN
@@ -97,6 +137,7 @@
                   NEWVALUE = NEWESTIMATE(I,J,H,PHIS,LENGTH)
                 END IF
                 HIGHESTCHANGE = HIGHESTCHANGEFUN(PHIS(I,J),NEWVALUE,HIGHESTCHANGE)
+                AVGCHANGE = AVGCHANGE + ABS(PHIS(I,J)-NEWVALUE)/SIZE
                 WRITE (*,*) "OLD VALUE IS ", PHIS(I,J)
                 PHIS(I,J) = NEWVALUE
                 WRITE (*,*) "NEW VALUE IS ", PHIS(I,J)
@@ -107,7 +148,7 @@
         open (unit=out_unit,file="results.txt",action="write",status="replace")
         DO I=1,LENGTH
           DO J=1,LENGTH
-              WRITE (OUT_UNIT,'(F10.5)') PHIS(I,J)
+              WRITE (OUT_UNIT,'(F20.5)') PHIS(I,J)
           END DO
         END DO
         close (out_unit)
