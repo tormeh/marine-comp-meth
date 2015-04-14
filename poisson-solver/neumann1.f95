8bfbea41cc44df06f945335ace0afcb62636b5d2
tormeh <tormod.hellen@gmail.com>
2015-04-14 20:49:21 +0200

    works pretty darn well now!

--- poisson-solver/neumann1.f95
+++ poisson-solver/neumann1.f95
@@ -4,8 +4,12 @@
         INTEGER X,Y
         REAL H
         REAL F1
-        F1=12-12*X*H-12*Y*H
-        !F1=(6-12*X*H)*(3*Y*Y*H*H-2*Y*Y*Y*H*H*H) + (3*X*X*H*H-2*X*X*X*H*H*H)*(6-12*Y*H)
+        REAL :: XC
+        REAL :: YC
+        XC = (X-1)*H
+        YC = (Y-1)*H
+        F1=12-12*XC-12*YC
+        !F1=(6-12*XC)*(3*YC**2-2*YC**3) + (3*XC**2-2*XC**3)*(6-12*YC)
         RETURN
       END FUNCTION
            
@@ -14,7 +18,12 @@
         INTEGER :: X
         INTEGER :: Y
         REAL :: H
-        ANALYTICAL = 3*X*X*H*H + 3*Y*Y*H*H - 2*X*X*X*H*H*H - 2*Y*Y*Y*H*H*H
+        REAL :: XC
+        REAL :: YC
+        XC = (X-1)*H
+        YC = (Y-1)*H
+        ANALYTICAL = 3*XC**2 + 3*YC**2 - 2*XC**3 - 2*YC**3
+        !ANALYTICAL = (3*YC**2 - 2*YC**3)*(3*XC**2 - 2*XC**3)
         RETURN
       END FUNCTION
       
@@ -63,6 +72,91 @@
           BOUNDARY = NEWVALUE
         ELSE
           BOUNDARY = NEWESTIMATE(X,Y,H,PHIS,LENGTH)
+        END IF
+        RETURN
+      END FUNCTION
+      
+      FUNCTION BOUNDARYTEST(X,Y,H,PHIS,LENGTH)
+        INTEGER X
+        INTEGER  Y
+        REAL H
+        INTEGER LENGTH
+        REAL NEWVALUE
+        REAL :: PHIS(LENGTH,LENGTH)
+        REAL BOUNDARYTEST
+        REAL NEWESTIMATE
+        REAL F1
+        IF (X==1 .AND. Y==1) THEN
+          PHIS(1,2)=0.0
+          PHIS(2,1)=0.0
+          PHIS(2,2)=0.0
+          BOUNDARYTEST=0.0
+        ELSE IF (X==1 .AND. Y==LENGTH) THEN
+          BOUNDARYTEST = (PHIS(2,LENGTH-1) + PHIS(2,LENGTH) + PHIS(1,LENGTH-1))/3.0
+        ELSE IF (X==LENGTH .AND. Y==1) THEN
+          BOUNDARYTEST = (PHIS(LENGTH-1,2) + PHIS(LENGTH,2) + PHIS(LENGTH-1,1))/3.0
+        ELSE IF (X==LENGTH .AND. Y==LENGTH) THEN
+          BOUNDARYTEST = (PHIS(LENGTH-1,LENGTH-1) + PHIS(LENGTH-1,LENGTH) + PHIS(LENGTH,LENGTH-1))/3.0
+        ELSE IF (X==1) THEN
+          NEWVALUE = (1.0/2.0)*(PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTEST = (PHIS(X+1,Y) + NEWVALUE)/2.0
+        ELSE IF (X==LENGTH) THEN
+          NEWVALUE = (1.0/2.0)*(PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTEST = (PHIS(X-1,Y) + NEWVALUE)/2.0
+        ELSE IF (Y==1) THEN
+          NEWVALUE = (1.0/2.0)*(PHIS(X+1,Y)+PHIS(X-1,Y)-H*H*F1(X,Y,H))
+          BOUNDARYTEST = (PHIS(X,Y+1) + NEWVALUE)/2.0
+        ELSE IF (Y==LENGTH) THEN
+          NEWVALUE = (1.0/2.0)*(PHIS(X+1,Y)+PHIS(X-1,Y)-H*H*F1(X,Y,H))
+          BOUNDARYTEST = (PHIS(X,Y-1) + NEWVALUE)/2.0
+        END IF
+        RETURN
+      END FUNCTION
+      
+      FUNCTION BOUNDARYTESTT(X,Y,H,PHIS,LENGTH)
+        INTEGER X
+        INTEGER  Y
+        REAL H
+        INTEGER LENGTH
+        REAL NEWVALUE
+        REAL :: PHIS(LENGTH,LENGTH)
+        REAL BOUNDARYTESTT
+        REAL NEWESTIMATE
+        REAL F1
+        REAL :: ORIGINDEMAND
+        IF (X==1 .AND. Y==1) THEN
+          !PHIS(1,2) = (H*H*F1(1,1,H)-2*PHIS(2,1))/2
+          !PHIS(2,1) = (H*H*F1(1,1,H)-2*PHIS(1,2))/2
+          PHIS(1,2)=0.0
+          PHIS(2,1)=0.0
+          PHIS(2,2)=0.0
+          BOUNDARYTESTT=0.0
+        ELSE IF (X==1 .AND. Y==2) THEN
+          ORIGINDEMAND = (H*H*F1(1,1,H)-2*PHIS(2,1))/2
+          NEWVALUE = (1.0/4.0)*(2*PHIS(X+1,Y)+PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = PHIS(1,2) !ORIGINDEMAND !(ORIGINDEMAND + NEWVALUE)/2
+        ELSE IF (X==2 .AND. Y==1) THEN
+          ORIGINDEMAND = (H*H*F1(1,1,H)-2*PHIS(1,2))/2
+          NEWVALUE = (1.0/4.0)*(PHIS(X+1,Y)+2*PHIS(X,Y+1)+PHIS(X-1,Y)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = PHIS(2,1) !ORIGINDEMAND !(ORIGINDEMAND + NEWVALUE)/2
+        ELSE IF (X==1 .AND. Y==LENGTH) THEN
+          BOUNDARYTESTT = (1.0/4.0)*(2*PHIS(X+1,Y)+2*PHIS(X,Y-1)-H*H*F1(X,Y,H))
+        ELSE IF (X==LENGTH .AND. Y==1) THEN
+          BOUNDARYTESTT = (1.0/4.0)*(2*PHIS(X,Y+1)+2*PHIS(X-1,Y)-H*H*F1(X,Y,H))
+        ELSE IF (X==LENGTH .AND. Y==LENGTH) THEN
+          BOUNDARYTESTT = (1.0/4.0)*(2*PHIS(X-1,Y)+2*PHIS(X,Y-1)-H*H*F1(X,Y,H))
+        ELSE IF (X==1) THEN
+          NEWVALUE = (1.0/4.0)*(2*PHIS(X+1,Y)+PHIS(X,Y+1)+PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = NEWVALUE
+        ELSE IF (X==LENGTH) THEN
+          NEWVALUE = (1.0/4.0)*(PHIS(X,Y+1)+2*PHIS(X-1,Y)+PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = NEWVALUE
+        ELSE IF (Y==1) THEN
+          NEWVALUE = (1.0/4.0)*(PHIS(X+1,Y)+2*PHIS(X,Y+1)+PHIS(X-1,Y)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = NEWVALUE
+        ELSE IF (Y==LENGTH) THEN
+          NEWVALUE = (1.0/4.0)*(PHIS(X+1,Y)+PHIS(X-1,Y)+2*PHIS(X,Y-1)-H*H*F1(X,Y,H))
+          BOUNDARYTESTT = NEWVALUE
         END IF
         RETURN
       END FUNCTION
@@ -191,6 +285,8 @@
         REAL BOUNDARY
         REAL BOUNDARYPURE
         REAL BOUNDARYNEIGHBOR
+        REAL BOUNDARYTEST
+        REAL BOUNDARYTESTT
         REAL ANALYTICAL
         INTEGER I,J
         REAL HIGHESTCHANGEFUN
@@ -206,6 +302,8 @@
         LOWHIGHESTCHANGE = 20.0
         HIGHESTCHANGE = 10.0
         
+        WRITE (*,*) "LENGTH*H IS ", (LENGTH*H)
+        
         DO I=1,LENGTH
           DO J=1,LENGTH
             PHIS(I,J) = RAND(0)*10
@@ -213,7 +311,7 @@
         END DO
         
         NUMITERATIONS = 0
-        DO WHILE ((LOWHIGHESTCHANGE/HIGHESTCHANGE) > 1.0 .OR. (LOWAVGCHANGE/AVGCHANGE) > 1.0 )
+        DO WHILE ((LOWHIGHESTCHANGE/HIGHESTCHANGE) > 1.0 .OR. (LOWAVGCHANGE/AVGCHANGE) > 1.0 .OR. NUMITERATIONS < 30000)
           NUMITERATIONS = NUMITERATIONS + 1
           LOWAVGCHANGE = LOWCHANGE(LOWAVGCHANGE, AVGCHANGE)
           LOWHIGHESTCHANGE = LOWCHANGE(LOWHIGHESTCHANGE, HIGHESTCHANGE)
@@ -222,14 +320,14 @@
           DO I=1,LENGTH
             DO J=1,LENGTH
                 IF (I==1 .OR. I==LENGTH .OR. J==1 .OR. J==LENGTH) THEN
-                  NEWVALUE = BOUNDARY(I,J,H,PHIS,LENGTH)
+                  NEWVALUE = BOUNDARYTESTT(I,J,H,PHIS,LENGTH)
                 !ELSE IF (I==2 .OR. I==LENGTH-1 .OR. J==2 .OR. J==LENGTH-1) THEN
                   !NEWVALUE = BOUNDARYNEIGHBOR(I,J,H,PHIS,LENGTH)
                 ELSE
                   NEWVALUE = NEWESTIMATE(I,J,H,PHIS,LENGTH)
                 END IF
                 HIGHESTCHANGE = HIGHESTCHANGEFUN(PHIS(I,J),NEWVALUE,HIGHESTCHANGE)
-                AVGCHANGE = AVGCHANGE + ABS(PHIS(I,J)-NEWVALUE)/SIZE
+                AVGCHANGE = AVGCHANGE + ((PHIS(I,J)-NEWVALUE)**2)/SIZE
                 PHIS(I,J) = NEWVALUE
             END DO
           END DO
@@ -243,26 +341,26 @@
         DO I=1,LENGTH
           DO J=1,LENGTH
           IF (I==1 .OR. I==LENGTH .OR. J==1 .OR. J==LENGTH) THEN
-            NUMAVGERROR = NUMAVGERROR + ABS(PHIS(I,J)-BOUNDARYPURE(I,J,H,PHIS,LENGTH))/SIZE
+            NUMAVGERROR = NUMAVGERROR + ((PHIS(I,J)-BOUNDARYTEST(I,J,H,PHIS,LENGTH))**2)/SIZE
           ELSE
-            NUMAVGERROR = NUMAVGERROR + ABS(PHIS(I,J)-NEWESTIMATE(I,J,H,PHIS,LENGTH))/SIZE
+            NUMAVGERROR = NUMAVGERROR + ((PHIS(I,J)-NEWESTIMATE(I,J,H,PHIS,LENGTH))**2)/SIZE
           END IF
           END DO
         END DO
-        WRITE (*,*) "AVERAGE NUMERICALLY ESTIMATED ERROR IS ", NUMAVGERROR
+        WRITE (*,*) "AVERAGE SQUARE NUMERICALLY ESTIMATED ERROR IS ", NUMAVGERROR
         
         !performance metrics. Analytically determined error is printed
         AVGERROR = 0.0
         DO I=1,LENGTH
           DO J=1,LENGTH
-          AVGERROR = AVGERROR + ABS(PHIS(I,J)-ANALYTICAL(I,J,H))/SIZE
+          AVGERROR = AVGERROR + ((PHIS(I,J)-ANALYTICAL(I,J,H))**2)/SIZE
           END DO
         END DO
-        WRITE (*,*) "AVERAGE ANALYTICALLY DETERMINED ERROR IS ", AVGERROR
+        WRITE (*,*) "AVERAGE SQUARE ANALYTICALLY DETERMINED ERROR IS ", AVGERROR
         
         open (unit=out_unit,file="results.txt",action="write",status="replace")
-        WRITE (OUT_UNIT,'(I3)') LENGTH
-        WRITE (OUT_UNIT,'(I3)') LENGTH
+        WRITE (OUT_UNIT,'(I4)') LENGTH
+        WRITE (OUT_UNIT,'(I4)') LENGTH
         DO I=1,LENGTH
           DO J=1,LENGTH
               WRITE (OUT_UNIT,'(F10.5)') PHIS(I,J)
